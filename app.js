50f5e65ff9a7f79e73e5f5d29c2a28072b3da285
Edson Hirano <edson.hriano28@gmail.com>
2022-01-09 11:53:56 -0300

    Projeto 50%

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function buscar() {
+    window.alert("Função de busca ainda não implementada");
+}
+
+function goToLoginOrRegister() {
+    window.alert("Função de login e cadastro ainda não implementada");
+}
